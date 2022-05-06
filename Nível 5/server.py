3cdf8eea6d7f9c39dbb962ff54e705c55265f60d
samuelfr19 <79268066+samuelfr19@users.noreply.github.com>
2022-05-06 14:04:54 +0100

    nivel 5 finalizado

---
+++ Nível 5/server.py
@@ -0,0 +1,53 @@
+"""
+ Implements a simple socket server
+
+"""
+
+import socket
+
+# Define socket host and port
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 8000
+
+# Create socket
+while True:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((SERVER_HOST, SERVER_PORT))
+
+    server_socket.listen(1)
+    print('Listening on port %s ...' % SERVER_PORT)
+
+    # Wait for client connections
+
+    client_connection, client_address = server_socket.accept()
+
+    # Print message from client
+    msg = client_connection.recv(1024).decode()
+
+    try:
+        result = eval(msg)
+        print('Received:', result)
+
+        print("returning message... " + str(result))
+        client_connection.send(str(result).encode())
+    except:
+        if msg == "exit":
+            server_socket.close()
+            print("exiting ...")
+            break
+
+        print("Mensagem inválida para avaliação!")
+        print("returning message... " + msg)
+        client_connection.send(msg.encode())
+
+
+
+
+
+
+    # Close client connection
+    client_connection.close()
+
+    # Close socket
+    server_socket.close()
